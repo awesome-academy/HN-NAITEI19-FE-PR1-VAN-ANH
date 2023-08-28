74e6153fdffc0d27b40eb7cc1def89024ad83f11
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2023-08-28 21:43:55 +0700

    Add header, footer for homepage & add register page & add login page

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+        // Xử lý sau
+});
+
+function goBackToHomePage() {
+    window.location.href = "index.html";
+}
+
+function goToRegisterPage() {
+    window.location.href = "register.html";
+}
